5bcb7eac8b6aa287494f8dbebdaa1b1cb06d44f2
yrvan <yrvan.naya@gnmail.com>
2024-03-20 23:50:53 -0400

    Ajouter la méthode stopperTemps() à la classe Modele

--- Modele.py
+++ Modele.py
@@ -6,6 +6,13 @@ class Modele:
         self.parent = parent
         self.environnement= None
         self.date=None
+        #self.debuterTemps() #initialisation du temps seulement si c'est le model final
         
     def debuterTemps(self):
-        self.date = Time() #class codé en sorte qu'il n'y ai qu'un seul repre temporelle
+        self.date = Time() #class codé en sorte qu'il n'y ai qu'un seul repere temporelle
+        
+    def stopperTemps(self):
+        self.date.thread.stop() #stopper la mise a jour du repere temporelle pour pouvoir en creer un autre plus tard
+        self.date.thread.join()  # Attendre que le thread se termine proprement
+
+        
