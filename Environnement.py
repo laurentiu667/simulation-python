75adc1bfb0022ea4ab05a3cc2eff5065a85fd6fc
yrvan <yrvan.naya@gnmail.com>
2024-03-22 15:28:30 -0400

    Ajouter une interface graphique pour afficher l'environnement (test)

--- Environnement.py
+++ Environnement.py
@@ -3,8 +3,37 @@ from time import time
 import Animal
 import Vegetal
 from helper import Helper as hp
-
+from tkinter import *
+import Time
 from abc import ABC, abstractmethod
+from Animal import Cerf, Castor
+from Vegetal import Sapin
+
+class Vu:
+    def __init__(self):
+        self.root = Tk()
+        self.root.title("Environnement")
+        self.canvas = Canvas(self.root, width=500, height=500)
+        self.canvas.pack()
+        self.afficher()
+        
+        
+        
+    def afficher(self):
+        canvas_width = self.canvas.winfo_width()
+        canvas_height = self.canvas.winfo_height()
+        
+        taille_plante = 50
+        x1 = (canvas_width - taille_plante) / 2
+        y1 = (canvas_height - taille_plante) / 2
+        x2 = x1 + taille_plante
+        y2 = y1 + taille_plante
+
+        self.canvas.create_text(x1, y1, text="Plante")
+
+    def actualiser(self):
+        self.root.mainloop()
+
 class Environnement:
     def __init__(self):
         self.biome = None
@@ -20,12 +49,10 @@ class Environnement:
         self.isSoleil = None
         self.testAnimaux = [Animal.Cerf, Animal.Castor]
         self.testVegetaux = [Vegetal.Sapin, Vegetal.Sapin]
+        
 
 
     def CatastropheMeteorologique(self):
-   
-
-
         def orages(self):
             pass
 
@@ -38,11 +65,11 @@ class Environnement:
 
 class Biome:
     def __init__(self, name, orage, night,climate, average_temperature, precipitation, dominant_flora, dominant_fauna):
-        self.name = name  # Nom du biome, par exemple "Forêt tropicale", "Toundra", etc.
-        self.climate = climate  # Type de climat, par exemple "Tropical", "Aride", etc.
-        self.average_temperature = average_temperature  # Température moyenne en degrés Celsius
-        self.precipitation = precipitation  # Précipitations annuelles moyennes en mm
-        self.dominant_flora = dominant_flora  # Les formes de vie végétales dominantes
+        self.name = name
+        self.climate = climate
+        self.average_temperature = average_temperature
+        self.precipitation = precipitation
+        self.dominant_flora = dominant_flora
         self.dominant_fauna = dominant_fauna
         self.orage = orage
         self.night = night
@@ -50,21 +77,20 @@ class Biome:
 
 class Saison:
     def __init__(self, name, temperature, precipitation, humdite, dominant_flora, dominant_fauna):
-        self.name = name  # Nom de la saison, par exemple "Hiver", "Mousson", etc.
-        self.temperature = temperature  # Température moyenne en degrés Celsius
-        self.precipitation = precipitation  # Précipitations moyennes en mm
-        self.dominant_flora = dominant_flora  # Les formes de vie végétales dominantes
-        self.dominant_fauna = dominant_fauna  # Les formes de vie animales dominantes
+        self.name = name
+        self.temperature = temperature
+        self.precipitation = precipitation
+        self.dominant_flora = dominant_flora
+        self.dominant_fauna = dominant_fauna
         self.humidite = humdite
-        
 
 
 class Ete(Saison, Environnement, Biome):
     def __init__(self):
         self.saison = Saison("Ete", 0, 0, "incounnu", "incounnu")
         self.biome = Biome("Ete", False, "incounnu", 0, 0, "incounnu", "incounnu")
+
     def comportement_estivale(self, temp, precip, humidite):
-     
         if self.saison.temperature > 30:
             if self.saison.precipitation != 0: 
                 for animal in self.testAnimaux:
@@ -73,54 +99,21 @@ class Ete(Saison, Environnement, Biome):
                 if self.saison.humidite > 50:
                     for vegetal in self.testVegetaux:
                         vegetal.humidite += 20
-                else:
-                    pass
-            elif self.saison.precipitation > 0:  # Avec précipitation
+            elif self.saison.precipitation > 0:
                 for animal in self.testAnimaux:
                     animal.soif -= animal.soif * 0.05
                 for vegetal in self.testVegetaux:
-                    vegetal.humidite += 0.15  
+                    vegetal.humidite += 15  
                 if self.saison.humidite > 75:
                     for vegetal in self.testVegetaux:
                         vegetal.photosynthese += 10 
-                else:
-                    pass
-            elif self.biome.orage:
-                for animal in self.testAnimaux:
-                    # doit trouver la cachette la plus proche
-                    if animal.cacher is True:
-                        # doit faire en sorte de se deplacer vers la cachete
-                        pass
-            elif self.saison.precipitation == 0 and self.biome.night:
-                #Rentre dans la fonction seDeplacer de la classe Animaux
-                # for animal in self.testAnimaux:
-                #     animal.deplacer()
-                # ???????
-                for animal in self.testAnimaux:
-                    animal
 
 
-        
-                
-    
-
-class Hiver(Saison):
-    def __init__(self):
-        self.saison = Saison("Hiver", 0, 0, "incounnu", "incounnu")
-
-class Printemps(Saison):
-    def __init__(self):
-        self.saison = Saison("Printemps", 0, 0, "incounnu", "incounnu")
-
-class Automne(Saison):
-    def __init__(self):
-        self.saison = Saison("Automne", 0, 0, "incounnu", "incounnu")
-
-
-if  __name__ == "__main__":
+if __name__ == "__main__":
     e = Environnement()
-    e.CatastropheMeteorologique()
+    vue = Vu()
+    
+    vue.actualiser()
 
-
-# la generation des animaux pourrait dependre du dominant fauna de la saison en cours et du biome
-# en fonction des methodes propre au saison et aux biomes je rajouterais des sousclasse de saison et biomes
+    
+    
