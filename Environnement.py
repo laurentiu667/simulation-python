039931e33a0124bbde13b7b4e0cfa820c039fc5e
laurentiu667 <dilionlaur144@gmail.com>
2024-03-22 16:22:58 -0400

    test vue environnement

--- Environnement.py
+++ Environnement.py
@@ -15,21 +15,76 @@ class Vu:
         self.root.title("Environnement")
         self.canvas = Canvas(self.root, width=500, height=500)
         self.canvas.pack()
-        self.afficher()
+    
         
         
         
     def afficher(self):
-        canvas_width = self.canvas.winfo_width()
-        canvas_height = self.canvas.winfo_height()
         
-        taille_plante = 50
-        x1 = (canvas_width - taille_plante) / 2
-        y1 = (canvas_height - taille_plante) / 2
-        x2 = x1 + taille_plante
-        y2 = y1 + taille_plante
+        biome_frame = Frame(self.root, background="red")
+        biome_frame.pack()
+        biome_titre = Label(biome_frame, text="Biomes :")
+        biome_titre.pack(side='left')
+        biome_info = Label(biome_frame, text="Temple")
+        biome_info.pack(side='left')
 
-        self.canvas.create_text(x1, y1, text="Plante")
+        date_frame = Frame(self.root)
+        date_frame.pack()
+        date_titre = Label(date_frame, text="Date :")
+        date_titre.pack(side='left')
+        date_info = Label(date_frame, text="2024-04-22")
+        date_info.pack(side='left')
+
+        heure_frame = Frame(self.root)
+        heure_frame.pack()
+        heure_titre = Label(heure_frame, text="Heure :")
+        heure_titre.pack(side='left')
+        heure_info = Label(heure_frame, text="11:52")
+        heure_info.pack(side='left')
+
+        isSoleil_frame = Frame(self.root)
+        isSoleil_frame.pack()
+        isSoleil_titre = Label(isSoleil_frame, text="Soleil :")
+        isSoleil_titre.pack(side='left')
+        isSoleil_info = Label(isSoleil_frame, text="Nuageux")
+        isSoleil_info.pack(side='left')
+
+        temperature_frame = Frame(self.root)
+        temperature_frame.pack()
+        temperature_titre = Label(temperature_frame, text="Temperature :")
+        temperature_titre.pack(side='left')
+        temperature_info = Label(temperature_frame, text="23")
+        temperature_info.pack(side='left')
+
+        humidite_frame = Frame(self.root)
+        humidite_frame.pack()
+        humidite_titre = Label(humidite_frame, text="Humidite :")
+        humidite_titre.pack(side='left')
+        humidite_info = Label(humidite_frame, text="36%")
+        humidite_info.pack(side='left')
+
+        isOrage_frame = Frame(self.root)
+        isOrage_frame.pack()
+        isOrage_titre = Label(isOrage_frame, text="Orage :")
+        isOrage_titre.pack(side='left')
+        isOrage_info = Label(isOrage_frame, text="Il faut nuageux")
+        isOrage_info.pack(side='left')
+
+        isTornade_frame = Frame(self.root)
+        isTornade_frame.pack()
+        isTornade_titre = Label(isTornade_frame, text="Tornade :")
+        isTornade_titre.pack(side='left')
+        isTornade_info = Label(isTornade_frame, text="Une tornade en approche")
+        isTornade_info.pack(side='left')
+
+        isTempetes_tropicale_frame = Frame(self.root)
+        isTempetes_tropicale_frame.pack()
+        isTempetes_tropicale_titre = Label(isTempetes_tropicale_frame, text="Tempetes tropicale :")
+        isTempetes_tropicale_titre.pack(side='left')
+        isTempetes_tropicale_info = Label(isTempetes_tropicale_frame, text="Tempetes tropicale")
+        isTempetes_tropicale_info.pack(side='left')
+
+
 
     def actualiser(self):
         self.root.mainloop()
@@ -113,6 +168,8 @@ if __name__ == "__main__":
     e = Environnement()
     vue = Vu()
     
+    
+    vue.afficher()
     vue.actualiser()
 
     
